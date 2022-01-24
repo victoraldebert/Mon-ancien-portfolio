8cbe52a298e25652f95acd9308dfb1d90b9997ef
Victor <91267367+victoraldebert@users.noreply.github.com>
2022-01-24 09:06:02 +0100

    Scroll animation

---
+++ scroll.js
@@ -0,0 +1,58 @@
+const sr = ScrollReveal();                  //Méthode de l'effet
+
+
+sr.reveal('.col-md-5', {
+    origin: 'top',
+    distance: '50px',
+    scale: 0.87,
+    reset: true,
+    duration: 2250  
+});    
+
+
+sr.reveal('.col-lg-4', {
+    origin: 'top',
+    distance: '50px',
+    reset: true,
+
+    duration: 2250
+});    
+
+
+
+sr.reveal('.featurette-heading', {
+    origin: 'bottom',
+    distance: '50px',
+    reset: true,
+
+    duration: 2000
+});
+
+
+
+
+sr.reveal('.lead', {
+    origin: 'bottom',
+    distance: '40px',
+    reset: true,
+
+    duration: 2250
+});
+
+
+sr.reveal('.btn-secondary', {
+    origin: 'bottom',
+    distance: '30px',
+    reset: true,
+
+    duration: 2750
+});
+
+
+sr.reveal('.navbar-brand', {
+    origin: 'left',
+    distance: '30px',
+    reset: true,
+
+    duration: 2750
+});
